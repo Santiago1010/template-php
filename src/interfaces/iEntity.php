7bc4990432c6ebbcdb0093aecac102ea8f07db64
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2022-12-27 14:51:50 -0500

    Fatal error: Uncaught TypeError: Api\Models\UsersModel::createuserDB(): Argument #1 () must be of type Api\Models\Api\Models\Entities\Users, Api\Models\Entities\Users given, called in C:\wamp64\www\template-php\src\controllers\UsersController.php on line 33 and defined in C:\wamp64\www\template-php\src\models\UsersModel.php on line 29

--- src/interfaces/iEntity.php
+++ src/interfaces/iEntity.php
@@ -4,9 +4,9 @@ namespace Api\Interfaces;
 
 interface iEntity {
 
-	public function create(string $query); string
-	public function read(string $query): string
-	public function update(string $query): string
-	public function delete(string $query): string
+	public function create(string $query): string;
+	public function read(string $query): string;
+	public function update(string $query): string;
+	public function delete(string $query): string;
 
 }
