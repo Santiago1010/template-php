2d50565a55ef26ec363436b87c450362a39d33db
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2022-12-27 08:50:46 -0500

    Se ha completado la creación y eliminación de entidades y modelos.

---
+++ src/functions/commands/delete/EntityCommand.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Api\Functions\Commands\Delete;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{ InputInterface, InputArgument };
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Helper\QuestionHelper;
+
+use Api\Traits\Files;
+
+class EntityCommand extends Command {
+	
+	use Files;
+
+	protected static $defaultName = 'delete:entity';
+
+	protected function configure() {
+		$this->setDescription('Eliminar una entidad')->setHelp('Este comando te permite eliminar una entidad...')->addArgument('name', InputArgument::REQUIRED, 'Nombre de la entidad');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		// Obtener el nombre del archivo ingresado por el usuario
+		$name = $this->getEntityName($input);
+
+		// Definir la ruta local donde se guardará el archivo de la función
+		$this->defineLocalRoute('./src/models/entities/');
+
+		if ($this->confirmDelete($input, $output) && $this->safeDelete($name, $output)) {
+			// Eliminar el archivo.
+			$this->deleteFile("./src/models/entities/{$name}.php") ? $output->writeln("El archivo {$name} ha sido eliminado.") : $output->writeln("No se ha encontrado el archivo './src/models/entities/{$name}.php'");
+		}
+
+		return Command::SUCCESS;
+	}
+
+	private function safeDelete(string $name, OutputInterface $output): bool {
+		$safe = [''];
+
+		if (in_array(strtolower($name), $safe)) {
+			$output->writeln("La entidad '{$name}' no puede ser eliminado, ya que forma parte del núcleo del framework.");
+			return false;
+		}
+
+		return true;
+	}
+
+	private function confirmDelete(InputInterface $input, OutputInterface $output): bool {
+		$questionHelper = $this->getHelper('question');
+
+		$question = new ConfirmationQuestion('¿Deseas eliminar esta entidad? (y/n) ', false);
+
+
+		if ($questionHelper->ask($input, $output, $question)) {
+			return true;
+		} else {
+			$output->writeln('No se ha eliminado la entidad.');
+			return false;
+		}
+	}
+
+	private function getEntityName(InputInterface $input): string {
+		$name = $input->getArgument('name');
+
+		// Hacer un TitleCase al nombre
+		$name = ucwords($name);
+
+		// Retirar caracteres especiales.
+		$name = str_replace([' ', '_', '-', ':'], '', $name);
+
+		return $name;
+	}
+
+		
+}
---
+++ src/functions/commands/delete/ModelCommand.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Api\Functions\Commands\Delete;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\{ InputInterface, InputArgument };
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Helper\QuestionHelper;
+
+use Api\Traits\Files;
+
+class ModelCommand extends Command {
+	
+	use Files;
+
+	protected static $defaultName = 'delete:model';
+
+	protected function configure() {
+		$this->setDescription('Eliminar un modelo')->setHelp('Este comando te permite eliminar un modelo...')->addArgument('name', InputArgument::REQUIRED, 'Nombre del modelo');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		// Obtener el nombre del archivo ingresado por el usuario
+		$name = $this->getModelName($input);
+
+		// Definir la ruta local donde se guardará el archivo de la función
+		$this->defineLocalRoute('./src/models/');
+
+		if ($this->confirmDelete($input, $output) && $this->safeDelete($name, $output)) {
+			// Eliminar el archivo.
+			$this->deleteFile("./src/models/{$name}.php") ? $output->writeln("El archivo {$name} ha sido eliminado.") : $output->writeln("No se ha encontrado el archivo './src/models/{$name}.php'");
+		}
+
+		return Command::SUCCESS;
+	}
+
+	private function safeDelete(string $name, OutputInterface $output): bool {
+		$safe = [''];
+
+		if (in_array(strtolower($name), $safe)) {
+			$output->writeln("El modelo '{$name}' no puede ser eliminado, ya que forma parte del núcleo del framework.");
+			return false;
+		}
+
+		return true;
+	}
+
+	private function confirmDelete(InputInterface $input, OutputInterface $output): bool {
+		$questionHelper = $this->getHelper('question');
+
+		$question = new ConfirmationQuestion('¿Deseas eliminar este modelo? (y/n) ', false);
+
+
+		if ($questionHelper->ask($input, $output, $question)) {
+			return true;
+		} else {
+			$output->writeln('No se ha eliminado el modelo.');
+			return false;
+		}
+	}
+
+
+	private function getModelName(InputInterface $input) {
+		$name = $input->getArgument('name');
+
+		// Remover espacios y caracteres especiales
+		$name = preg_replace('/\s+/', '', $name);
+		$name = preg_replace('/[^A-Za-z0-9]/', '', $name);
+
+		// Verificar si el nombre ya contiene 'Controller', 'Controllers' o variantes
+		if (preg_match('/Model$|Models$/i', $name) !== 1) {
+			$name .= 'Model';
+		} else {
+			$name = preg_replace('/Model$|Models$/i', 'Model', $name);
+		}
+
+		// Hacer un TitleCase al nombre
+		$name = ucwords($name);
+
+		return $name;
+	}
+
+		
+}
